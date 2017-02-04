4fa37f3d46952691a23c255e56a2aa25e3619845
Jokerzzs <490619376@qq.com>
2017-02-04 17:06:46 +0800

    Update index.jsx

--- webparts/webparta/index.jsx
+++ webparts/webparta/index.jsx
@@ -1,6 +1,45 @@
-﻿import Name from '../../components/componenta/ca.jsx';
-if (document.getElementById('app')) {
-    ReactDOM.render(
-        <Name></Name>,
-        document.getElementById('app'));
-}
+import { render } from 'react-dom';
+import SliderFrame from '../../components/slider/sliderFrame.jsx';
+
+import SliderLinks from '../../components/slider/sliderLinks.jsx';
+import WebPartFrame from '../../components/Common/webPartFrame.jsx';
+
+
+
+const LINK_DATA =[[{'itemhref': 'www.baidu.com', 'icon': 'aaa', 'title': 'aaa description' },
+                    {'itemhref': 'www.baidu.com', 'icon': 'aaa', 'title': 'aaa description' },
+                    {'itemhref': 'www.baidu.com', 'icon': 'aaa', 'title': 'aaa description' },
+                    {'itemhref': 'www.baidu.com', 'icon': 'aaa', 'title': 'aaa description' },
+                    {'itemhref': 'www.baidu.com', 'icon': 'aaa', 'title': 'aaa description' },
+                    {'itemhref': 'www.baidu.com', 'icon': 'aaa', 'title': 'aaa description' },
+                    {'itemhref': 'www.baidu.com', 'icon': 'aaa', 'title': 'aaa description' }],
+                [{'itemhref': 'www.baidu.com', 'icon': 'aaa', 'title': 'aaa description' },
+                    {'itemhref': 'www.baidu.com', 'icon': 'aaa', 'title': 'aaa description' },
+                    {'itemhref': 'www.baidu.com', 'icon': 'aaa', 'title': 'aaa description' },
+                    {'itemhref': 'www.baidu.com', 'icon': 'aaa', 'title': 'aaa description' },
+                    {'itemhref': 'www.baidu.com', 'icon': 'aaa', 'title': 'aaa description' },
+                    {'itemhref': 'www.baidu.com', 'icon': 'aaa', 'title': 'aaa description' },
+                    {'itemhref': 'www.baidu.com', 'icon': 'aaa', 'title': 'aaa description' }]];
+
+render(
+<WebPartFrame
+    title={"Frequently Accessed List"}
+    hasMore={false}
+    link={"wwww.baidu.com"}
+    hasTopLine ={false}
+>
+    
+  <SliderFrame
+itemCount={LINK_DATA.length}
+speed={1.2}
+delay={2.1}
+pause={true}
+autoplay={false}
+dots={true}
+arrows={true}
+>
+<SliderLinks items={LINK_DATA} />
+</SliderFrame>
+</WebPartFrame>,
+document.getElementById('acclinks')
+);
