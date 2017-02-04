7f055b64445fccf2db1817c8e5efea040d5ca5b3
Alex-Ji <jikai2008@sina.com>
2017-02-04 16:07:44 +0800

    Change call webpart function

--- webparts/Slider/index.jsx
+++ webparts/Slider/index.jsx
@@ -2,22 +2,45 @@ import { render } from 'react-dom';
 import SliderFrame from '../../components/slider/sliderFrame.jsx';
 import SliderPics from '../../components/slider/sliderPics.jsx';
 
+function sliderRender() {
 
-const IMAGE_DATA = [{ 'src': '../../components/img/image1.jpg', 'alt': 'image1', 'itemhref': 'www.baidu.com', 'title': 'aaa', 'description': 'aaa description' },
-{ 'src': '../../components/img/image2.jpg', 'alt': 'image2', 'itemhref': 'www.sina.com.cn', 'title': 'bbb', 'description': '' },
-{ 'src': '../../components/img/image3.jpg', 'alt': 'image3', 'itemhref': 'www.sohu.com', 'title': '', 'description': 'desc2' }];
+  let IMAGE_DATA = [{ 'src': '../../components/img/image1.jpg', 'alt': 'image1', 'itemhref': 'www.baidu.com', 'title': 'aaa', 'description': 'aaa description' },
+  { 'src': '../../components/img/image2.jpg', 'alt': 'image2', 'itemhref': 'www.sina.com.cn', 'title': 'bbb', 'description': '' },
+  { 'src': '../../components/img/image3.jpg', 'alt': 'image3', 'itemhref': 'www.sohu.com', 'title': '', 'description': 'desc2' }];
 
-render(
-  <SliderFrame
-    itemCount={IMAGE_DATA.length}
-    speed={1.2}
-    delay={2.1}
-    pause={true}
-    autoplay={false}
-    dots={true}
-    arrows={true}
-  >
-    <SliderPics items={IMAGE_DATA} />
-  </SliderFrame>,
-  document.getElementById('slider')
-);
+  let param = {
+    items: [],
+    speed: 1,
+    delay: 2,
+    pause: true,
+    autoplay: false,
+    dots: true,
+    arrows: true,
+    url: ''
+  };
+
+  param.url = '';
+  param.speed = 1;
+  param.delay = 1;
+  param.pause = true;
+  param.autoplay = false;
+  param.dots = true;
+  param.arrows = true;
+
+  render(
+    <SliderFrame
+      itemCount={IMAGE_DATA.length}
+      speed={1.2}
+      delay={2.1}
+      pause={true}
+      autoplay={false}
+      dots={true}
+      arrows={true}
+    >
+      <SliderPics items={IMAGE_DATA} />
+    </SliderFrame>,
+    document.getElementById('slider')
+  );
+}
+
+global.sliderRender = sliderRender;
