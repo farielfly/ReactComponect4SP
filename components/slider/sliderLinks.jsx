a27ac0870abaa8ae5587006738b4b3ec64b6469c
Jokerzzs <490619376@qq.com>
2017-02-06 13:17:37 +0800

    slider linklist

--- components/slider/sliderLinks.jsx
+++ components/slider/sliderLinks.jsx
@@ -8,15 +8,15 @@ export default class SliderLinks extends React.Component {
         let count = items.length;
         let width = 100 / count + '%';
 
-        let itemCollection = this.props.items.map((items, idx) => {
-            if (items.length) {
-                let itemNodes = items.map((item, idn) => {
-                    return <a href={item.src} key={'link' + idn} className="item-link">
-                        <span>{item.title}</span>
-                        <span>{item.icon}</span>
-                    </a>
-                })
-                return <li className="slider-link" style={{ width: width }}>{itemNodes}</li>
+        let itemCollection = this.props.items.map((items,idc) =>{
+            if(typeof(items.length) === 'number'){
+                let itemNodes = items.map((item,idn) =>{
+                    return <a key={'link'+idn} href={item.itemhref} className="item-link link-bgcolor">
+                       <span>{item.title}</span>
+                       <span>{item.icon}</span>
+                   </a>
+                });
+                return (<li className="slider-link" key={'coll'+idc} style={{width: width }}>{itemNodes}</li>);
             }
         })
 
