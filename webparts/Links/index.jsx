a0884be26a3c269124750b1ac9e0a6b315b1d6bd
Alex-Ji <jikai2008@sina.com>
2017-02-07 11:04:31 +0800

    Change WebPart name

---
+++ webparts/Links/index.jsx
@@ -0,0 +1,47 @@
+import { render } from 'react-dom';
+import SliderFrame from '../../components/slider/sliderFrame.jsx';
+
+import SliderLinks from '../../components/slider/sliderLinks.jsx';
+import WebPartFrame from '../../components/Common/webPartFrame.jsx';
+
+
+
+const LINK_DATA = [[{ 'itemhref': 'www.baidu.com', 'icon': 'aaa', 'title': 'aaa description' },
+{ 'itemhref': 'www.baidu.com', 'icon': 'aaa', 'title': 'aaa description' },
+{ 'itemhref': 'www.baidu.com', 'icon': 'aaa', 'title': 'aaa description' },
+{ 'itemhref': 'www.baidu.com', 'icon': 'aaa', 'title': 'aaa description' },
+{ 'itemhref': 'www.baidu.com', 'icon': 'aaa', 'title': 'aaa description' },
+{ 'itemhref': 'www.baidu.com', 'icon': 'aaa', 'title': 'aaa description' },
+{ 'itemhref': 'www.baidu.com', 'icon': 'aaa', 'title': 'aaa description' }],
+[{ 'itemhref': 'www.baidu.com', 'icon': 'aaa', 'title': 'aaa description' },
+{ 'itemhref': 'www.baidu.com', 'icon': 'aaa', 'title': 'aaa description' },
+{ 'itemhref': 'www.baidu.com', 'icon': 'aaa', 'title': 'aaa description' },
+{ 'itemhref': 'www.baidu.com', 'icon': 'aaa', 'title': 'aaa description' },
+{ 'itemhref': 'www.baidu.com', 'icon': 'aaa', 'title': 'aaa description' },
+{ 'itemhref': 'www.baidu.com', 'icon': 'aaa', 'title': 'aaa description' },
+{ 'itemhref': 'www.baidu.com', 'icon': 'aaa', 'title': 'aaa description' }]];
+
+if (document.getElementById('acclinks')) {
+render(
+    <WebPartFrame
+        title={"Frequently Accessed List"}
+        hasMore={false}
+        link={"wwww.baidu.com"}
+        hasTopLine={false}
+    >
+
+        <SliderFrame
+            itemCount={LINK_DATA.length}
+            speed={1.2}
+            delay={2.1}
+            pause={true}
+            autoplay={false}
+            dots={true}
+            arrows={true}
+        >
+            <SliderLinks items={LINK_DATA} />
+        </SliderFrame>
+    </WebPartFrame>,
+    document.getElementById('acclinks')
+);
+}
