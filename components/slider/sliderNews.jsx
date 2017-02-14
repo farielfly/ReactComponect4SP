a7bdf819be673333fd3afa64a3cf67ebd21e8bed
Alex-Ji <jikai2008@sina.com>
2017-02-14 11:10:10 +0800

    news

--- components/slider/sliderNews.jsx
+++ components/slider/sliderNews.jsx
@@ -1,28 +1,28 @@
 import React, { Component } from 'react';
 
- class NewItem extends Component {
+class NewItem extends Component {
     constructor(props) {
         super(props);
     }
 
     render() {
-       let height =  100 /this.props.maxCount+ '%';
-        
+        let height = 100 / this.props.maxCount + '%';
+
         return <div>
             {
-                this.props.children.map((item,idx)=>{
-                    return <div key={'news'+idx} style={{height: height}}>
-                    <div className='newContent'><img src={item.src} /></div>
-                    <div className='newContent'><a href={item.href}>{item.value}</a></div>
-                    <div className='newContent'><span className='icon-clock'></span>{item.date}</div>
+                this.props.children.map((item, idx) => {
+                    return <div key={'news' + idx} style={{ height: height }}>
+                        <div className='newContent'><img src={item.src} /></div>
+                        <div className='newContent'><a href={item.href}>{item.value}</a></div>
+                        <div className='newContent'><span className='icon-clock'></span>{item.date}</div>
                     </div>
-                })           
+                })
             }
         </div>
     }
 }
 
-export default class News extends Component{
+export default class News extends Component {
     constructor(props) {
         super(props);
     }
@@ -30,10 +30,10 @@ export default class News extends Component{
     render() {
         let { items, left, speed, nowLocal, maxCount  } = this.props;
         let count = items.length;
-        let width = 100 / items.length + '%';         
+        let width = 100 / items.length + '%';
 
-        let itemNodes = this.props.items.map((item, idx) => {  
-            return <li style={{width: width}} ><NewItem children={item.props.children} key={idx} maxCount={maxCount}/></li>;
+        let itemNodes = this.props.items.map((item, idx) => {
+            return <li style={{ width: width }} ><NewItem children={item.props.children} key={idx} maxCount={maxCount} /></li>;
         });
 
         return (
@@ -43,7 +43,7 @@ export default class News extends Component{
                 width: count * 100 + "%"
             }}>
                 {itemNodes}
-            </ul>       
+            </ul>
         );
-    }    
+    }
 }
--- webparts/News/index.jsx
+++ webparts/News/index.jsx
@@ -5,78 +5,93 @@ import SliderItem from '../../components/slider/sliderNews.jsx';
 import $ from 'jquery';
 
 function newsRender(config) {
-  const data = [{ 'src': '../../components/img/image1.jpg', 'href': 'www.baidu.com', 'date': '01/03/2017 10:00AM', 'value': 'test' },
-  { 'src': '../../components/img/image2.jpg', 'href': 'www.baidu.com', 'date': '01/03/2017 10:00AM', 'value': 'test' },
-  { 'src': '../../components/img/image3.jpg', 'href': 'www.baidu.com', 'date': '01/03/2017 10:00AM', 'value': 'test' },
-  { 'src': '../../components/img/image1.jpg', 'href': 'www.baidu.com', 'date': '01/03/2017 10:00AM', 'value': 'test' },
-  { 'src': '../../components/img/image1.jpg', 'href': 'www.baidu.com', 'date': '01/03/2017 10:00AM', 'value': 'test' }];
+    const data = [{ 'src': '../../components/img/image1.jpg', 'href': 'www.baidu.com', 'date': '01/03/2017 10:00AM', 'value': 'test' },
+    { 'src': '../../components/img/image2.jpg', 'href': 'www.baidu.com', 'date': '01/03/2017 10:00AM', 'value': 'test' },
+    { 'src': '../../components/img/image3.jpg', 'href': 'www.baidu.com', 'date': '01/03/2017 10:00AM', 'value': 'test' },
+    { 'src': '../../components/img/image1.jpg', 'href': 'www.baidu.com', 'date': '01/03/2017 10:00AM', 'value': 'test' },
+    { 'src': '../../components/img/image1.jpg', 'href': 'www.baidu.com', 'date': '01/03/2017 10:00AM', 'value': 'test' }];
 
-  var maxCount = 3;
-  let param = { url: '', speed: 1, delay: 1, pause: true, autoplay: false, dots: true, arrows: true };
+    var maxCount = 3;
+    let param = { url: '', speed: 1, delay: 1, pause: true, autoplay: false, dots: true, arrows: true, listurl: '', webparttitle: '' };
 
-  function renderUI(data, param) {
-    var itemNodes = [];
+    function renderUI(data, param) {
+        var itemNodes = [];
 
-    for (let i = 0, len = data.length; i < len; i += maxCount) {
-      let cell = Math.ceil(data.length / maxCount);
-      itemNodes.push(<SliderItem children={data.slice(i, i + maxCount)} count={cell} idx={i} />);
+        for (let i = 0, len = data.length; i < len; i += maxCount) {
+            let cell = Math.ceil(data.length / maxCount);
+            itemNodes.push(<SliderItem children={data.slice(i, i + maxCount)} count={cell} idx={i} />);
+        }
+
+        if (document.getElementById('news')) {
+            render(
+                <WebPartFrame
+                    title={param.webparttitle}
+                    hasMore={true}
+                    link={param.listurl}
+                    hasTopLine={true}
+                    >
+                    <SliderFrame
+                        itemCount={itemNodes.length}
+                        speed={param.speed}
+                        delay={param.delay}
+                        pause={param.pause}
+                        autoplay={param.autoplay}
+                        dots={param.dots}
+                        arrows={param.arrows}
+                        >
+                        <SliderItem items={itemNodes} maxCount={maxCount} />
+                    </SliderFrame>
+                </WebPartFrame>,
+                document.getElementById('news')
+            );
+        }
     }
 
-    if (document.getElementById('news')) {
-      render(
-        <WebPartFrame
-          title={"NEWS & ANNOUNCEMENTS"}
-          hasMore={true}
-          link={"wwww.baidu.com"}
-          hasTopLine={true}
-          >
-          <SliderFrame
-            itemCount={itemNodes.length}
-            speed={param.speed}
-            delay={param.delay}
-            pause={param.pause}
-            autoplay={param.autoplay}
-            dots={param.dots}
-            arrows={param.arrows}
-            >
-            <SliderItem items={itemNodes} maxCount={maxCount} />
-          </SliderFrame>
-        </WebPartFrame>,
-        document.getElementById('news')
-      );
+    function loadData(param) {
+        $.ajax({
+            type: "GET",
+            url: config.url,
+            headers: {
+                "Accept": "application/json;odata=verbose",
+                "Content-Type": "application/json;odata=verbose",
+            },
+            dataType: "json",
+            data: {},
+            config: param,
+            async: false,
+            success: function(dataInput) {
+                var data = new Array();
+                for (var i = 0, l = dataInput.d.results.length; i < l; i++) {
+                    data.push({
+                        'src': dataInput.d.results[i].ACSImageUrl,
+                        'date': dataInput.d.results[i].ACSPublishedDate,
+                        'href': this.config.listurl + '/DispForm.aspx?ID=' + dataInput.d.results[i].ID,
+                        'value': dataInput.d.results[i].Title
+                    })
+                }
+                renderUI(data, this.config);
+            },
+            error: function(data) {
+
+            }
+        });
     }
-  }
 
-  function loadData(param) {
-    $.ajax({
-      type: "post",
-      url: config.url,
-      data: {},
-      config: param,
-      datatype: "xml",
-      async: false,
-      success: function (data) {
-        renderUI(data, this.config);
-      },
-      error: function (data) {
+    if (config && !config.debug) {
+        param.speed = config.speed ? config.speed : 1;
+        param.delay = config.delay ? config.delay : 1;
+        param.pause = config.pause ? config.pause : true;
+        param.autoplay = config.autoplay ? config.autoplay : false;
+        param.dots = config.dots ? config.dots : true;
+        param.arrows = config.arrows ? config.arrows : true;
+        param.listurl = config.listurl ? config.listurl : '';
+        param.webparttitle = config.webparttitle ? config.webparttitle : '';
+        loadData(param);
 
-      }
-    });
-  }
-
-  if (config && !config.debug) {
-    param.speed = config.speed ? config.speed : 1;
-    param.delay = config.delay ? config.delay : 1;
-    param.pause = config.pause ? config.pause : true;
-    param.autoplay = config.autoplay ? config.autoplay : false;
-    param.dots = config.dots ? config.dots : true;
-    param.arrows = config.arrows ? config.arrows : true;
-    loadData(param);
-
-  }
-  else {
-    renderUI(data, param);
-  }
+    }
+    else {
+        renderUI(data, param);
+    }
 }
 
 global.newsRender = newsRender;
