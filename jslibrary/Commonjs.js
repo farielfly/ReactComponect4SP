4c5da16c4dfb1c55f374f830ee09cc172a069548
yuyuzeze <yuyuzeze@gmail.com>
2017-02-20 18:30:41 +0800

    webpart

--- jslibrary/Commonjs.js
+++ jslibrary/Commonjs.js
@@ -1,5 +1,4 @@
-﻿
-var AIC;
+﻿var AIC;
 (function (AIC) {
     "use strict";
     AIC.Global = function () {
@@ -23,12 +22,12 @@ var AIC;
     };
 
     function changeStyle() {
-        
+
         $('#titleAreaRow>div:nth-child(2)').html("").attr('id', 'navigation');
         var config = { url: _spPageContextInfo.siteAbsoluteUrl + "/_layouts/15/APPSAICSolution/NavigationProvider.aspx" }
         window.NavigationRender(config);
-        
-        $.get('/_vti_bin/APPSAICSolution/CommonService.svc/GetFooter', function (data) {            
+
+        $.get('/_vti_bin/APPSAICSolution/CommonService.svc/GetFooter', function (data) {
             if (data) {
                 addFooter(data.Footer.Links, data.Footer.Search)
             }
@@ -39,6 +38,15 @@ var AIC;
             }
         })
         $('#titleAreaRow>div:nth-child(2)').css('display', 'table-cell');
+
+        $('#contentRow').resize(function () {
+            if ($('#s4-bodyContainer').height() > $('#s4-workspace').height()) {
+                $('#acs-footer')[0].className = 'acs-footer acs-footer-relative';
+            }
+            else {
+                $('#acs-footer')[0].className = 'acs-footer';
+            }
+        });
     }
 
     function addFooter(footerArray, search) {
@@ -46,7 +54,7 @@ var AIC;
         if ($('#s4-bodyContainer').height() > $('#s4-workspace').height()) {
             footerClass += ' acs-footer-relative';
         }
-        var html = '<div class="' + footerClass + '"><span class="acs-link">';
+        var html = '<div id="acs-footer" class="' + footerClass + '"><span class="acs-link">';
         footerArray.map(function (item) {
             html += '<a href="' + item.Url + '" target="_blank">' + item.Name + '</a>';
         })
@@ -63,23 +71,8 @@ var AIC;
         window.open(searchUrl + '?k=' + $('.acs-search input').val(), 'search');
     }
 
-    AIC.wordLimit =function(query){
-        $(query).each(function(){
-            //var copyThis = $(this.cloneNode(true)).hide().css({
-            //    'position': 'absolute',
-            //    'width': 'auto',
-            //    'overflow': 'visible'
-            //});
-            //$(this).after(copyThis);
-            //if (copyThis.width() > $(this).width() * 2) {
-            //    $(this).text($(this).text().substring(0, $(this).html().length - 4));
-            //    $(this).html($(this).html() + '¡­');
-            //    copyThis.remove();
-            //    wordLimit();
-            //} else {
-            //    copyThis.remove();
-            //    return;
-            //}
+    AIC.wordLimit = function (query) {
+        $(query).each(function () {
             var fontSize = parseInt($(this).css('font-size').replace('px',''));
             while ($(this).height() > fontSize * 2 * 1.4) {
                 $(this).text($(this).text().replace(/(\s)*([a-zA-Z0-9]+|\W)(\.\.\.)?$/, "..."));
