16133f828b663c9404357b5a7843a55ada5c9645
yue.zhao <0412.edo@gmail.com>
2017-03-09 17:03:43 +0800

    add event calendar

--- components/Common/eventCalendar.jsx
+++ components/Common/eventCalendar.jsx
@@ -42,7 +42,7 @@ export default class EventCalendar extends React.Component {
                     {itemCollection}
                 </div>
                 <div className="acs-eventcalendar">
-                    
+
                 </div>
             </WebPartFrame>
         );
