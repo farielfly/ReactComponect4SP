55969dd6563c937a2ad077eae511d111ea80dc26
xiayuanxi <47274234@qq.com>
2017-03-10 13:54:05 +0800

    fix bug

--- components/Article/articleLike.jsx
+++ components/Article/articleLike.jsx
@@ -2,12 +2,13 @@ export default class ArticleLike extends React.Component {
     constructor(props) {
         super(props);
         this.state = { mLikes: props.like, mHasLiked: props.hasLiked }
+        this.likeClicked.bind(this);
     }
 
     likeClicked(event) {
         var likes = this.state.mHasLiked ? this.state.mLikes - 1 : this.state.mLikes + 1;
-        var hasLiked = this.state.mHasLiked;
-        this.setState({ mLikes: likes, mHasLiked: !hasLiked });
+        var hasLiked = this.
+        this.setState({ mLikes: likes, mHasLiked: !mHasLiked });
 
     }
 
@@ -15,7 +16,7 @@ export default class ArticleLike extends React.Component {
         var likes = this.state.mLikes;
         return (
             <div className={"acs-articleframe-like"}>
-                <div className={"acs-articleframe-like-icon"} onClick={this.likeClicked.bind(this)}></div>
+                <div className={"acs-articleframe-like-icon"} onClick={this.likeClicked}></div>
                 <div className={"acs-articleframe-like-title"}>{likes}</div>
             </div>
         );
