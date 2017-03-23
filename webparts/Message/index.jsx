8e32c087d30522a5536603b13908d8753fe4072b
yue.zhao <0412.edo@gmail.com>
2017-03-23 11:48:41 +0800

    connect with server

--- webparts/Message/index.jsx
+++ webparts/Message/index.jsx
@@ -6,7 +6,7 @@ function messageRender(config) {
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     const weeks = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     let param = { title: 'What You Should Know' };
-    let data = { title: 'townhall 2017', date: new Date(2017, 2, 28), picSrc: '../../components/img/image1.jpg' };
+    let data = { Title: 'townhall 2017', Date: new Date(2017, 2, 28), PictureUrl: '../../components/img/image1.jpg',Content:'Save the Date' };
 
     function formatDate(date) {
         return weeks[date.getDay()] + ", " + date.getDate() + " " + months[date.getMonth()] + " " + date.getFullYear();
@@ -21,10 +21,10 @@ function messageRender(config) {
                     link={""}
                     hasTopLine={false}>
                     <div className="acs-message">
-                        <img src={data.picSrc}></img>
-                        <div className="acs-message-title">{data.title}</div>
-                        <div className="acs-message-date">{formatDate(data.date)}</div>
-                        <a className="acs-message-btn" href="" onClick="">Save the Date</a>
+                        <img src={data.PictureUrl}></img>
+                        <div className="acs-message-title">{data.Title}</div>
+                        <div className="acs-message-date">{formatDate(new Date(data.Date))}</div>
+                        <div className="acs-message-content">{data.Content}</div>
                     </div>
                 </WebPartFrame>,
                 document.getElementById('message')
@@ -32,6 +32,9 @@ function messageRender(config) {
         }
     }
 
+    if (config && !config.debug) {
+        data = config.msg;
+    }
     renderUI(param, data);
 }
 
