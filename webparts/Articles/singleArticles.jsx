81e44139c58155b313de9e9dd751982d510275d4
Jokerzzs <490619376@qq.com>
2017-03-24 15:52:58 +0800

    Update and rename signleArticles.jsx to singleArticles.jsx

---
+++ webparts/Articles/singleArticles.jsx
@@ -0,0 +1,35 @@
+import { render } from 'react-dom';
+import $ from 'jquery';
+import ArticleTitle from '../../components/article/articleTitle.jsx';
+import ArticlePicture from '../../components/article/articlePicture.jsx';
+import ArticleHeadline from '../../components/article/articleHeadline.jsx';
+import ArticleIntro from '../../components/article/articleIntro.jsx';
+import ArticleTime from '../../components/article/articleTime.jsx';
+import ArticleLike from '../../components/article/articleLike.jsx';
+
+
+export default class SingleArticle extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        let {hasTitle,hasIntro,hasTime,hasLike,dataItem,itemWidth} = this.props;
+        let intro = hasIntro ? (<ArticleIntro intro={dataItem.Intro}></ArticleIntro>) : null;
+        let time = hasTime ? (<ArticleTime time={dataItem.Time}></ArticleTime>) : null;
+        let like = hasLike ? (<ArticleLike like={dataItem.Like}></ArticleLike>) : null;
+
+        
+        return (
+            <ArticleTitle title={dataItem.Title} width={itemWidth} hasTitle={hasTitle}>
+                <ArticlePicture picturePath={dataItem.PicturePath}></ArticlePicture>
+                <div className="asc-article-content">
+                    <ArticleHeadline headline={dataItem.Headline}></ArticleHeadline>
+                    {intro}
+                    {time}
+                    {like}
+                </div>
+            </ArticleTitle>
+        );
+    }
+}
