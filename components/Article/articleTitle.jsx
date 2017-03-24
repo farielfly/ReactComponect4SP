2c9184233dd45d9d73af12faba2fd72e49c0c542
Jokerzzs <490619376@qq.com>
2017-03-24 15:47:22 +0800

    Update articleTitle.jsx

--- components/Article/articleTitle.jsx
+++ components/Article/articleTitle.jsx
@@ -2,17 +2,37 @@ export default class ArticleTitle extends React.Component {
     constructor(props) {
         super(props);
     }
+  
+    handleResize() {  
+        if(window.document.body.offsetWidth <768){
+            this.refs.articleframe.style.width = "100%";
+        }
+        else{
+            this.refs.articleframe.style.width = this.props.width+"%";
+        }
+    } 
+    
+    componentDidMount() {  
+        window.addEventListener('resize', this.handleResize.bind(this));
+        this.handleResize();
+    }  
+    
+    componentWillUnmount() {  
+        window.removeEventListener('resize', this.handleResize.bind(this));  
+    } 
 
     render() {
-        let { title, iconPath, link} = this.props;
-
+        let {hasTitle, title, iconPath, link,width} = this.props;
+        let lineHead = !hasTitle? null:(<div className={"acs-articleframe-header"}>
+                                            <div className={"acs-articleframe-header-icon"}></div>
+                                            <div className={"acs-articleframe-header-title"}>{title}</div>
+                                        </div>);
         return (
-            <div className={"acs-articleframe"} >
-                <div className={"acs-articleframe-header"}>
-                    <div className={"acs-articleframe-header-icon"}></div>
-                    <div className={"acs-articleframe-header-title"}>{title}</div>
-                </div>
+            <div ref="articleframe" className={"acs-articleframe"} style={{width:width+"%"}} >
+                <div>
+                {lineHead}
                 {this.props.children}
+                </div>
             </div>
         );
     }
