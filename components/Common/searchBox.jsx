8bf90b8949fcb764b00fcf5d493d7898e57b6602
yue.zhao <0412.edo@gmail.com>
2017-03-25 15:27:04 +0800

    search

--- components/Common/searchBox.jsx
+++ components/Common/searchBox.jsx
@@ -1,26 +1,21 @@
 export default class SearchBox extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { searchContent: '' }
-        // this.onSearchClick = this.onSearchClick.bind(this);
     }
 
-    onSearchClick(event) {
+    onSearchClick() {
         let inputValue = this.refs.mSearchBox.value;
-        // alert("SearchContent: " + inputValue);
-        // this.setState({ searchContent: inputValue });
         window.open(this.props.searchUrl + inputValue);
     }
 
     render() {
         let { iconPosition, placeHolder, link } = this.props;
-        // let searchContent = this.state.searchContent;
 
         return (
             <div className={"acs-searchbox"}>
                 <div className={"acs-searchbox-headericon"} style={{ backgroundPosition: iconPosition }}></div>
-                <div className={"acs-searchbox-tailicon"} onClick={this.onSearchClick.bind(this, event)}></div>
                 <input className={"acs-searchbox-input"} type="text" placeholder={placeHolder} ref="mSearchBox"></input>
+                <div className={"acs-searchbox-tailicon"} onClick={this.onSearchClick.bind(this)}></div>
             </div>
         );
     }
