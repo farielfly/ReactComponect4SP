09122935a440a11f7c9aeb2519748f066e85140e
Jokerzzs <490619376@qq.com>
2017-03-27 16:18:34 +0800

    ntuc income navigation

--- components/Navigation/accordionFrame.jsx
+++ components/Navigation/accordionFrame.jsx
@@ -27,12 +27,18 @@ export default class AccordionFrame extends React.Component {
         let {accordionData,itemHeight,informationEvent,hasInfoPanel,isFirst} = this.props;
         let firstColor="#f58220",secondColor="#f2f4f6";
         let contentHeight =itemHeight==='auto'?itemHeight: accordionData.Items.length * itemHeight +"px";
-        let divisionIcon = accordionData.Items.length===0?'':(this.state.active?"acs-accordionitem-expandicon":"acs-accordionitem-retracticon");
+        let divisionIcon = '';
+        if(isFirst){
+             divisionIcon = accordionData.Items.length===0?'':(this.state.active?"acs-accordionitem-expandiconw":"acs-accordionitem-retracticonw");
+        }
+        else{
+            divisionIcon = accordionData.Items.length===0?'':(this.state.active?"acs-accordionitem-expandicong":"acs-accordionitem-retracticong");
+        }
         return (
             <div className="acs-accordion-item">
                 <div className={"acs-accordionitem-head" } onClick={this.handleShow.bind(this)} style={{"backgroundColor":isFirst?firstColor:secondColor}}>
-                    <span className={divisionIcon}></span>
                     <span className="acs-divisionhead-title" style={{color:isFirst?"#fff":"#333"}}>{this.props.accordionData.Title}</span>
+                    <span className={divisionIcon}></span>
                 </div>
                 <div className="acs-accordionitem-content" style={{ height: this.state.active ? contentHeight : "0" }}>
                     {this.props.children}
