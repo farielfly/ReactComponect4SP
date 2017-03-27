e78fc050d74c3b1bb6458b5f6cb184a0646045e2
yue.zhao <0412.edo@gmail.com>
2017-03-27 15:03:53 +0800

    search for mobile

--- webparts/QuickSearch/index.jsx
+++ webparts/QuickSearch/index.jsx
@@ -5,27 +5,38 @@ import SearchBox from '../../components/Common/searchBox.jsx'
 
 function searchRender(params) {
     if (!params) {
-        params = [{ IconPosition: '0 -62px', PlaceHolder: 'Find a person', SearchUrl:'https://www.google.com/#q=' },
-        { IconPosition: '-26px -62px', PlaceHolder: 'Search Everything', SearchUrl:'https://www.google.com/#q=' },
-        { IconPosition: '-52px -62px', PlaceHolder: 'Search all Corporate Documents', SearchUrl:'https://www.google.com/#q=' }];
+        params = [{ IconPosition: '0 -62px', PlaceHolder: 'Find a person', SearchUrl: 'https://www.google.com/#q=' },
+        { IconPosition: '-26px -62px', PlaceHolder: 'Search Everything', SearchUrl: 'https://www.google.com/#q=' },
+        { IconPosition: '-52px -62px', PlaceHolder: 'Search all Corporate Documents', SearchUrl: 'https://www.google.com/#q=' }];
     }
     renderUI(params);
 
     function renderUI(params) {
         if (document.getElementById('quicksearch')) {
             render(
-                <WebPartFrame title={"Quick Search"} hasMore={false} link={""} hasTopLine={false}>
-                    <div className={'acs-searchbox-container'}>
-                        {params.map(function (searchbox, i) {
-                            return <SearchBox
-                                key={i}
-                                iconPosition={searchbox.IconPosition}
-                                placeHolder={searchbox.PlaceHolder}
-                                searchUrl={searchbox.SearchUrl}>
-                            </SearchBox>
-                        })}
+                <div>
+                    <div id="search-forpc">
+                        <WebPartFrame title={"Quick Search"} hasMore={false} link={""} hasTopLine={false}>
+                            <div className={'acs-searchbox-container'}>
+                                {params.map(function (searchbox, i) {
+                                    return <SearchBox
+                                        key={i}
+                                        iconPosition={searchbox.IconPosition}
+                                        placeHolder={searchbox.PlaceHolder}
+                                        searchUrl={searchbox.SearchUrl}>
+                                    </SearchBox>
+                                })}
+                            </div>
+                        </WebPartFrame>
                     </div>
-                </WebPartFrame>,
+                    <div id="search-formobile">
+                        <SearchBox
+                            iconPosition={params[1].IconPosition}
+                            placeHolder={params[1].PlaceHolder}
+                            searchUrl={params[1].SearchUrl}>
+                        </SearchBox>
+                    </div>
+                </div>,
                 document.getElementById('quicksearch')
             )
         }
