e91ecc2ac3e0ed3e4a6fb30f113211c788b4d8e9
Jokerzzs <490619376@qq.com>
2017-03-31 10:56:11 +0800

    Create singleArticles.jsx

---
+++ components/Article/singleArticles.jsx
@@ -0,0 +1,41 @@
+import { render } from 'react-dom';
+import $ from 'jquery';
+import ArticleTitle from './articleTitle.jsx';
+import ArticlePicture from './articlePicture.jsx';
+import ArticleHeadline from './articleHeadline.jsx';
+import ArticleIntro from './articleIntro.jsx';
+import ArticleTime from './articleTime.jsx';
+import ArticleLike from './articleLike.jsx';
+
+
+export default class SingleArticle extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    openItem(url){
+        window.open(url);
+    }
+
+    render(){
+        let {hasTitle,hasIntro,hasTime,hasLike,dataItem,itemWidth} = this.props;
+        let intro = hasIntro ? (<ArticleIntro intro={dataItem.Intro}></ArticleIntro>) : null;
+        let time = hasTime ? (<ArticleTime time={dataItem.Time}></ArticleTime>) : null;
+        let like = hasLike ? (<ArticleLike like={dataItem.Like}></ArticleLike>) : null;
+
+        
+        return (
+            <ArticleTitle title={dataItem.Title} width={itemWidth} hasTitle={hasTitle} partUrl={dataItem.colleUrl}> 
+                <div onClick={this.openItem.bind(this,dataItem.ItemUrl)} style={{heigh:"auto"}}>
+                    <ArticlePicture picturePath={dataItem.PicturePath}></ArticlePicture>
+                    <div className="asc-article-content">
+                        <ArticleHeadline headline={dataItem.Headline}></ArticleHeadline>
+                        {intro}
+                        {time}
+                        {like}
+                    </div>
+                </div>
+            </ArticleTitle>
+        );
+    }
+}
