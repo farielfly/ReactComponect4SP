9134a51062d4167bdde4830cf0ab76f074155357
Shion3 <13354286876@qq.com>
2017-04-06 11:21:02 +0800

    10.	Add another property: "Office Location" in "Org structure Division Department Info List" and place the office location under division description, and above chief user info

--- components/accordion/infoPanel.jsx
+++ components/accordion/infoPanel.jsx
@@ -1,36 +1,37 @@
-export default class DivisionDetail extends React.Component{
-    constructor(props){
+export default class DivisionDetail extends React.Component {
+    constructor(props) {
         super(props);
     }
 
-    componentDidUpdate(){
+    componentDidUpdate() {
         let imgs = document.getElementsByClassName('ms-spimn-img');
-        for(var i=imgs.length-1;i>-1;i--){
-            imgs[i].setAttribute('sip',imgs[i].getAttribute('data-sip'));
-        }   
-        AIC.wordLimit(".acs-infohead-description",5)
+        for (var i = imgs.length - 1; i > -1; i--) {
+            imgs[i].setAttribute('sip', imgs[i].getAttribute('data-sip'));
+        }
+        AIC.wordLimit(".acs-infohead-description", 5)
         ProcessImn();
     }
 
-    render(){
+    render() {
 
-        if(this.props.infoData === null){
+        if (this.props.infoData === null) {
             return null;
         }
-        else{
-            return(
-                <div className="acs-infopanel" style={{display:!this.props.hasPanel?'none':'block'}}>
+        else {
+            return (
+                <div className="acs-infopanel" style={{ display: !this.props.hasPanel ? 'none' : 'block' }}>
                     <div className="acs-infopanel-head">
                         <div className="acs-infohead-commontitle">{this.props.commonTitle}</div>
                         <div className="acs-infohead-titlecontent">
-                           <div className="acs-infohead-title">{this.props.infoData.Title}</div>
-                           <div className="acs-infohead-description" title={this.props.infoData.Description}>{this.props.infoData.Description}</div>  
+                            <div className="acs-infohead-title">{this.props.infoData.Title}</div>
+                            <div className="acs-infohead-description" title={this.props.infoData.Description}>{this.props.infoData.Description}</div>
+                            <div className="acs-infohead-location">{this.props.infoData.Location}</div>
                         </div>
-                        
+
                     </div>
-                   {this.props.children}
+                    {this.props.children}
                     <div className="acs-jumplink"><div></div><a className="" href={this.props.infoData.Url}>{this.props.commonDes}</a></div>
-                    
+
                 </div>
             )
         }
