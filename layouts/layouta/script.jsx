25301162a4c67eab078b28f0722be62c17cc16f2
Alex-Ji <jikai2008@sina.com>
2017-04-07 18:35:21 +0800

    fix some bugs

--- layouts/layouta/script.jsx
+++ layouts/layouta/script.jsx
@@ -7,7 +7,7 @@ import PaginationTable from '../../components/gridtable/table.jsx';
 function staffDirectoryRender(config){
 
     let param={},data=[],canTurning = false;
-    let tableColumn=[{name:"Photo",width:5},{name:"Name",width:13},{name:"Division & Department",width:35},{name:"Designation",width:10},{name:"Email Me",width:17},{name:"Contact Me",width:20}];
+    let tableColumn=[{name:"Photo",width:5},{name:"Name",width:18},{name:"Division & Department",width:35},{name:"Designation",width:10},{name:"Email Me",width:17},{name:"Contact Me",width:15}];
 
     function renderUI(data) {
         if (document.getElementById('staff')) {
