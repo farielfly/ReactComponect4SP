9ed4946356fd1a6e3692c62b7946d109eb331383
Jokerzzs <490619376@qq.com>
2017-04-10 19:07:12 +0800

    Create tableBulk.jsx

---
+++ components/table/tableBulk.jsx
@@ -0,0 +1,31 @@
+export default class TableBulk extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    creatList(){
+        let {countInColumn, columnCount, listData} = this.props;
+        let tempList = [];
+        for(var i=0;i<columnCount;i++){
+            tempList[i] = listData.slice(countInColumn*i,countInColumn*(i+1));
+        }
+        return tempList;
+    }
+
+    render() {
+        let tempData = this.creatList();
+        let dataDiv = tempData.map((itemList,arrIndex)=>{
+                return <div key={"list"+arrIndex} className="acs-tablebulk-list">
+                    {itemList.map((item)=>{
+                        return React.cloneElement(this.props.children, {
+                                itemData:item
+                            });
+                    })}
+                </div>
+        })
+
+        return <div className="acs-tablebulk">
+            {dataDiv}
+        </div>
+    }
+}
