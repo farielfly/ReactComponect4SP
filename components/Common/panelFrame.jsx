6f9949d7442905f87b71d9a983978a6c6d04cfbd
AbelLiuKai <13478440986@163.com>
2017-04-28 09:07:58 +0800

    [abel.liu]Income Bug

--- components/Common/panelFrame.jsx
+++ components/Common/panelFrame.jsx
@@ -13,10 +13,11 @@ export default class PanelFrame extends React.Component{
         return(
             <div className="acs-panelframe" onClick={this.gotoDetails.bind(this,this.props.url)}>
                 <div className="acs-panelframe-title">
-                    <b>{this.props.title}</b>
-                    {this.props.msgCount>0?<div className="acs-panelframe-alert">{this.props.msgCount}</div>:null}
+					<div className="acs-panelframe-ico" style={{backgroundPosition:this.props.iconPosition}}>
+					   {this.props.msgCount>0?<div className="acs-panelframe-alert">{this.props.msgCount}</div>:null}
+					</div>
+                    <div>{this.props.title}</div>
                 </div>
-                <div className="acs-panelframe-ico" style={{backgroundPosition:this.props.iconPosition}}></div>
             </div>
         );
     }
