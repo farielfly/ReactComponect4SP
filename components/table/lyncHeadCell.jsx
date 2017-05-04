4c965fa169e4201f7ae8660c28dd7bdcd8131a8d
yuyuzeze <yuyuzeze@gmail.com>
2017-05-04 17:03:20 +0800

    webpart

--- components/table/lyncHeadCell.jsx
+++ components/table/lyncHeadCell.jsx
@@ -20,7 +20,7 @@ export default class LyncHeadCell extends React.Component{
                     </div>
                     <div className="acs-lynchead-personinfo">
                         <div>{itemData.Name}</div>
-                        <div>Title: {itemData.Title}</div>
+                        <div>Description: {itemData.Title}</div>
                     </div>
                 </div>
     }
