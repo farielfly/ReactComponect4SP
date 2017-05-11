d31614ee0d1524e0ce4871b35ca8d4d1c2c74384
AbelLiuKai <13478440986@163.com>
2017-05-11 16:23:46 +0800

    [abel.liu]Income Bug

--- webparts/Message/index.jsx
+++ webparts/Message/index.jsx
@@ -5,7 +5,7 @@ import WebPartFrame from '../../components/common/webPartFrame.jsx'
 function messageRender(config) {
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     const weeks = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    let param = { title: config.title, linkUrl: config.linkUrl };
+    let param = { title: config.title };
     let data = { Title: 'townhall 2017', Date: new Date(2017, 2, 28), PictureUrl: '../../components/img/image1.jpg', Content:'Save the Date'};
 
     function formatDate(date) {
@@ -18,11 +18,11 @@ function messageRender(config) {
                 <WebPartFrame
                     title={param.title}
                     hasMore={false}
-                    link={param.linkUrl}
+                    link={data.LinkUrl}
                     hasTopLine={false}>
                     <div className="acs-message">
-                        <a href={param.linkUrl}><img src={data.PictureUrl}></img></a>
-                        <div className="acs-message-title"><a href={param.linkUrl}>{data.Title}</a></div>
+                        <a href={data.LinkUrl}><img src={data.PictureUrl}></img></a>
+                        <div className="acs-message-title"><a href={data.LinkUrl}>{data.Title}</a></div>
                         <div className="acs-message-date">{formatDate(new Date(data.Date))}</div>
                         <div className="acs-message-content">{data.Content}</div>
                     </div>
